621e9466c73122f8561d8faf1f283f62c217e28a
Jing Wang AWS <wangjing@bu.ed>
2012-10-08 20:17:28 +0000

    fix bug of installation script for debain system(Ubuntu)

--- install/debian.sh
+++ install/debian.sh
@@ -3,6 +3,7 @@ sudo apt-get install python-dev
 sudo apt-get install python-numpy
 #sudo apt-get install python-scipy
 sudo apt-get install python-matplotlib
+sudo apt-get install python-setuptools
 python setup-dep.py
 
 
