85e7d0a481d139f8508a69a89fbba75540bc321f
wangjing@wangjing-ThinkPad-X230 <wangjing@wangjing-ThinkPad-X230>
2013-05-03 23:33:24 -0400

    Refactor DetectorLib.py

--- Detector/DetectorLib.py
+++ Detector/DetectorLib.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""A library of utility function that will be used by detectors
+""" A library of utility function that will be used by detectors
 """
 from sadit.util import np, Counter
 import operator
@@ -42,6 +42,32 @@ def adjust_pv(prob, eps):
         # return adjust_pv(prob2, eps / 2.0)
     return prob2
 
+def adjust_mat(P):
+    """  Adjust the joint distribution matrix P so that the minium value is >= EPS
+
+    Parameters
+    ---------------
+    P : np.array
+        joint distribution matrix P
+
+    Returns
+    --------------
+    ret : np.array
+        ajusted matrix.
+
+    Notes
+    --------------
+    the sum of all element in P is 1.
+
+    Examples
+    -------------------
+    >>> adjust_mat(np.array([[0, 0.5], [0, 0.5]]))
+    array([[  1.00000000e-20,   5.00000000e-01],
+           [  1.00000000e-20,   5.00000000e-01]])
+
+    """
+    shape = P.shape
+    return np.array(adjust_pv(P.ravel(), EPS)).reshape(shape)
 
 DF = lambda x,y:abs(x[0]-y[0]) * (256**3) + abs(x[1]-y[1]) * (256 **2) + abs(x[2]-y[2]) * 256 + abs(x[3]-y[3])
 
@@ -85,10 +111,6 @@ def I1(nu, mu):
 
     H = lambda x, y:x * log( x * 1.0 / y )
     return sum(H(a, b) for a, b in zip(nu, mu))
-
-def adjust_mat(P):
-    shape = P.shape
-    return np.array(adjust_pv(P.ravel(), EPS)).reshape(shape)
 
 def I2(J1, pi1, J2, pi2):
     """  calculate cross entropy for model-based empirical measure
@@ -148,21 +170,6 @@ def I2(J1, pi1, J2, pi2):
     if y < 0: import ipdb;ipdb.set_trace()
     return y
 
-# def quantize_state(x, nx, rg):
-#     minVal, maxVal = rg
-#     if minVal == maxVal:
-#         print '[warning] range size 0, rg: ', rg
-#         return x, [0]*len(x)
-
-#     stepSize = (maxVal - minVal) * 1.0 / (nx - 1 )
-#     g = []
-#     for ele in x:
-#         seq = int( (ele - minVal ) / stepSize + 0.5)
-#         if seq >= nx-1:
-#             seq = nx -1
-#         g.append(seq)
-#     return g
-
 def quantize_state(x, nx, rg):
     """ fast quantize_state that use numpy library
 
@@ -186,82 +193,6 @@ def quantize_state(x, nx, rg):
     quan = np.floor( (x - minVal) / stepSize )
     quan[quan == nx] = nx-1 # Fix for the largest value.
     return quan
-
-# if np:
-    # quantize_state = np_quantize_state
-
-# def get_dist_to_center(data, cluster, centerPt, DF):
-#     i = -1
-#     dis = []
-#     for x in data:
-#         i += 1
-#         cl = cluster[i]
-#         dis.append( DF( x, centerPt[cl] ) )
-
-#     return dis
-
-# def trans_data(flow):
-#     uniqueIP = set()
-#     srcIP = []
-#     srcIPVec = []
-#     flowSize = []
-#     time = []
-#     endTime = []
-#     for f in flow:
-#         uniqueIP.add( f['srcIP'] )
-#         srcIP.append( f['srcIP'] )
-#         srcIPVec.append( f['srcIPVec'] )
-#         flowSize.append( f['flowSize'] )
-#         time.append( f['t'] )
-#         endTime.append( f['endT'] )
-
-#     sortIdx = np.argsort(time)
-#     sortedSrcIP = []
-#     sortedFlowSize = []
-#     sortedTime = []
-#     sortedDur = []
-#     for idx in sortIdx:
-#         sortedSrcIP.append(srcIPVec[idx])
-#         sortedFlowSize.append(flowSize[idx])
-#         sortedTime.append(time[idx])
-#         sortedDur.append( endTime[idx] - time[idx] )
-
-#     return sortedSrcIP, sortedFlowSize, sortedTime, sortedDur
-
-        # show()
-
-# def f_hash(digit, level):
-#     '''This is just a hash function. to map a sequence to a unique number.
-#     The implemetation is: return digit[0] + digit[1] * level[0] + digit[2] * level[1] * level[0] ...
-#     '''
-#     if len(digit) == 3: # just try to make it faster for special case
-#         return digit[0] + digit[1] * level[0] + digit[2] * level[1] * level[0]
-#     else:
-#         value = digit[0]
-#         basis = [1]
-#         for i in xrange( len(digit) - 1 ):
-#             basis.append( basis[-1] * level[i] )
-#         value = sum( d*b for d, b zip(digit, basis) )
-
-        # for i in xrange(len(digit)-1):
-            # value += digit[i+1] * reduce(operator.mul, level[0:i+1])
-        # return value
-
-# basis_cache = dict() # ugly global cache of basis to accelerate the program
-# def get_feature_hash_list_old(F, level):
-#     ''' For each list of feature and corresponding quantized level.
-#     Get the hash value correspondingly
-#     '''
-#     import pdb;pdb.set_trace()
-#     if isinstance(level, list):
-#         level = tuple(level)
-#     basis = basis_cache.get(level, None)
-#     if basis is None:
-#         basis = [1]
-#         for i in xrange( len(level) - 1 ):
-#             basis.append( basis[-1] * level[i] )
-
-#     return [ sum( d*b for d, b in zip(digits, basis) ) for digits in zip(*F) ]
 
 cache_ = dict()
 def get_feature_hash_list(F, level):
@@ -293,138 +224,67 @@ def get_feature_hash_list(F, level):
         return get_feature_hash_list(F, level)
 
 
-from sadit.util import zeros
-def model_based_deprec(q_fea_vec, fea_QN):
-    '''estimate the transition probability. It has same input parameter with model_free.
-
-    - q_fea_vec :q_fea_vec is a list of list containing all the quantized feature in a window. len(q_fea_vec)
-               equals to the number of feature types. len(q_fea_vec[0]) equals to the number of flows in this
-               window.
-    - fea_QN : this is a list storing the quantized number for each feature.
-    '''
-    QLevelNum = reduce(operator.mul, fea_QN)
-    # P = np.zeros( (QLevelNum, QLevelNum) )
-    P = zeros( (QLevelNum, QLevelNum) )
-    fl = len(q_fea_vec[0])
-    mp = zeros((QLevelNum, ))
-    m_list = get_feature_hash_list(q_fea_vec, fea_QN)
-
-    for i in xrange(fl-1):
-        mp[ int(m_list[i]) ] += 1
-        P[ int(m_list[i]) ][ int(m_list[i+1]) ] += 1
-    mp[ int(m_list[fl-1]) ] += 1
-
-    # P = P * 1.0 / (fl-1)
-    for i in xrange(len(P)):
-        for j in xrange(len(P[0])):
-            P[i][j] = P[i][j] * 1.0 / (fl -1)
-    # mp = mp / fl
-    mp = [v*1.0/fl for v in mp]
-    return P, mp
-
-
 def model_based(q_fea_vec, fea_QN):
-    """ Estimate the transition probability.
+    """ estimate the transition probability. It has same input parameter with model_free.
 
-    It has same input parameter with model_free.
-
-    Parameters
-    -------------
     q_fea_vec : list of list
         q_fea_vec is a list of list containing all the quantized feature in a
         window. len(q_fea_vec) equals to the number of feature types.
         len(q_fea_vec[0]) equals to the number of flows in this window.
-    fea_QN : list
+    fea_QN : list of int
         this is a list storing the quantized number for each feature.
     """
-    if len(q_fea_vec[0]) < 2:
-        # print('these is only one sample in the range, cannot calculate ' \
-                # 'model based emperical measure')
-        return None, None
-
     QLevelNum = reduce(operator.mul, fea_QN)
-    m_list = get_feature_hash_list(q_fea_vec, fea_QN)
-    mp = zeros((QLevelNum, ))
-    ct = Counter(m_list)
-    total = len(m_list)
-    for k, v in ct.iteritems():
-        mp[int(k)] = v * 1.0 / total
-
-    P = zeros( (QLevelNum, QLevelNum) )
-    tran_pairs = itertools.izip(m_list[:-1], m_list[1:])
-    tran_ct = Counter(tran_pairs)
-    total_tran = total - 1
-    for (i, j), freq in tran_ct.iteritems():
-        P[int(i)][int(j)] = freq * 1.0 / total_tran
-
-    return P, mp
-
-def model_free_deprec(q_fea_vec, fea_QN):
-    '''estimate the probability distribution for model free case. It has same input parameters with model_based
-
-    - q_fea_vec :q_fea_vec is a list of list containing all the quantized feature in a window. len(q_fea_vec)
-               equals to the number of feature types. len(q_fea_vec[0]) equals to the number of flows in this
-               window.
-    - fea_QN : this is a list storing the quantized number for each feature.
-    '''
-    QLevelNum = reduce(operator.mul, fea_QN)
-    # P = np.zeros( (QLevelNum, ) )
-    P = [0] * QLevelNum
+    P = np.zeros( (QLevelNum, QLevelNum) )
     fl = len(q_fea_vec[0])
     m_list = get_feature_hash_list(q_fea_vec, fea_QN)
+    m_list = [int(v) for v in m_list]
 
-    for i in range(fl):
-        idx = int(m_list[i])
-        try:
-            P[idx] += 1
-        except Exception as e:
-            print e
-            import pdb; pdb.set_trace()
-    # P = P * 1.0 / fl
-    P = [v*1.0/fl for v in P]
-    assert(abs( sum(P) - 1.0) < 0.01)
-    # assert(abs( np.sum(np.sum(P, 0)) - 1.0) < 0.01)
-    return P
+    for i in xrange(fl-1):
+        P[ m_list[i] ][ m_list[i+1] ] += 1
+    P /= (fl - 1 )
+    return P, None
+    # return P
 
 def model_free(q_fea_vec, fea_QN):
-    '''estimate the probability distribution for model free case. It has same input parameters with model_based
+    """ estimate the probability distribution for model free case. It has same input parameters with model_based
 
-    - q_fea_vec :q_fea_vec is a list of list containing all the quantized feature in a window. len(q_fea_vec)
-               equals to the number of feature types. len(q_fea_vec[0]) equals to the number of flows in this
-               window.
-    - fea_QN : this is a list storing the quantized number for each feature.
-    '''
-    # import pdb;pdb.set_trace()
+    q_fea_vec : list of list
+        q_fea_vec is a list of list containing all the quantized feature in a
+        window. len(q_fea_vec) equals to the number of feature types.
+        len(q_fea_vec[0]) equals to the number of flows in this window.
+    fea_QN : list of int
+        this is a list storing the quantized number for each feature.
+    """
     QLevelNum = reduce(operator.mul, fea_QN)
-    P = [0] * QLevelNum
-    # fl = len(q_fea_vec[0])
+    P = np.zeros( (QLevelNum, ) )
+    fl = len(q_fea_vec[0])
     m_list = get_feature_hash_list(q_fea_vec, fea_QN)
+    m_list = [int(v) for v in m_list]
 
-    ct = Counter(m_list)
-    total = sum(ct.values())
-    try:
-        for k, v in ct.iteritems():
-            P[int(k)] = v * 1.0 / total
-    except:
-        import pdb;pdb.set_trace()
+    for v in m_list:
+        P[v] += 1
+    P /= fl
+    assert(abs( sum(P) - 1.0) < 0.01)
     return P
 
-if not Counter :
-    model_free = model_free_deprec
-    model_based = model_based_deprec
+def vector_quantize_states(fea_vec, fea_QN, fea_range, quan_flag=None):
+    """ Quantize a vector of numbers.
 
-def vector_quantize_states(fea_vec , fea_QN, fea_range, quan_flag=None):
-    '''Quantize a vector of numbers.
+    Parameters
+    ---------------------
+    fea_vec : a list of list
+        containing all the features in a window. len(fea_vec) equals to
+        the number of feature types. len(fea_vec[0]) equals to the number
+        of flows in this window.
+    fea_QN : list of int
+        quantized number for each feature. len(feaQn) equals to the number of feature types.
+    fea_range : a list of two-digit tupe
+        containing the range for each feature. the length for first diemension
+        equals to the number of feature types.  the length of the second
+        dimension is two.
 
-    - fea_vec : is a list of list containing all the features in a window. len(fea_vec)
-               equals to the number of feature types. len(fea_vec[0]) equals to the number of flows in this
-               window.
-    - fea_QN : quantized number for each feature. len(feaQn) equals to the number of feature types.
-    - fea_range : a list of two-digit tupe containing the range for each feature. the
-                 length for first diemension equals to the number of feature types.
-                 the length of the second dimension is two.
-
-    '''
+    """
     if not quan_flag: quan_flag = len(fea_QN) * [1]
     QS = lambda a, b, c, flag: quantize_state(a, b, c) if flag else a
     res = [ QS(a, b, c, flag) for a, b, c, flag in itertools.izip(fea_vec , fea_QN, fea_range, quan_flag) ]
@@ -433,8 +293,7 @@ def vector_quantize_states(fea_vec , fea_QN, fea_range, quan_flag=None):
 def SL(data, st, ed):
     return [d[st:ed] for d in data]
 
-def get_range(data):
-    return [ (min(x), max(x)) for x in data ]
+
 
 if __name__ == "__main__":
     import doctest
