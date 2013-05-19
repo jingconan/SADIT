747ccc42a667d80553fe9c1cdd221d952265b368
Jing Conan Wang <wangjing@bu.edu>
2013-05-19 19:22:49 -0400

    Fix error in FSDataVisualizer.py

--- tools/FSDataVisualizer.py
+++ tools/FSDataVisualizer.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python
 from __future__ import print_function, division
 import sys; sys.path.append('../')
+from Detector.Data import HDF_FS
+# from pylab import *
+import matplotlib.pyplot as plt
 
-from Detector.DataParser import RawParseData
-from pylab import *
+class Visualizer(HDF_FS):
+    # def __init__(self, f_name):
+    #     self.f_name = f_name
+    #     data, self.keys = RawParseData(f_name)
+    #     self.zip_data = zip(*data)
 
-class Visualizer(object):
-    def __init__(self, f_name):
-        self.f_name = f_name
-        data, self.keys = RawParseData(f_name)
-        self.zip_data = zip(*data)
-
-    def get(self, name):
-        idx = self.keys.index(name)
-        return self.zip_data[idx]
+    # def get(self, name):
+    #     idx = self.keys.index(name)
+    #     return self.zip_data[idx]
 
     def vis(self):
-        flow_size = [int(v) for v in self.get('flow_size')]
-        start_time = [float(v) for v in self.get('start_time')]
+        # flow_size = [int(v) for v in self.get('flow_size')]
+        # start_time = [float(v) for v in self.get('start_time')]
+
+        flow_size = self.get_rows('flow_size')
+        start_time = self.get_rows('start_time')
         min_t = min(start_time)
         start_time = [t-min_t for t in start_time]
-        plot(start_time, flow_size)
-        xlabel('time')
-        ylabel('flow size')
-        show()
+        plt.plot(start_time, flow_size)
+        plt.xlabel('time')
+        plt.ylabel('flow size')
+        plt.show()
         pass
 
 if __name__ == "__main__":
