5f638a784ed81eed6900f926bb3e0d36bc2fb69e
wangjing <wangjing AT bu.edu>
2013-11-27 16:49:40 -0500

    Add Benchmark

---
+++ Benchmarks/benDet.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from __future__ import print_function, division
+import sys
+sys.path.insert(0, "..")
+sys.path.insert(0, "../..")
+from Detector.Data import HDF_FS
+
+
+def main():
+    for i in xrange(50):
+        print('i', i)
+        HDF_FS('./n0_flow.txt')
+    print('done')
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
