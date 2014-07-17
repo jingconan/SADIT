de06b05ddb3966b07aaf0a8aa8342c8e5fc229df
Jing Conan Wang <hbhzwj@gmail.com>
2014-07-17 01:19:43 -0400

    remove para_comp and test since they are outdated

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-# ./sadit -e DetectExper -d ./Simulator/n0_flow.txt -m 2w --win_size=300
-# ./sadit -e DetectExper -d ./Simulator/n0_flow.txt -m art --win_size=300
-# ./sadit -e DetectExper -d ./Simulator/n0_flow.txt -m mfmb --win_size=300
-# ./run.py -e DetectExper -d ./Simulator/n0_flow.txt -m svm_fbf
-# ./run.py -e DetectExper -d ./Simulator/n0_flow.txt -m svm_temp
-# ./run.py -e DetectExper -d ./Simulator/n0_flow.txt -m auto --win_size=300
-# ./run.py -e DetectExper -d ./Simulator/n0_flow.txt -m period --win_size=300
