2df5f4cccc34f5a3fcdb51cba811cda3c9f57e83
Jing Conan Wang <hbhzwj@gmail.com>
2014-07-17 01:17:07 -0400

    Remove d3-plot-engine since it has been included in GAD

--- d3-plot-engine/server.js
+++
@@ -1,9 +0,0 @@
-var io = require('socket.io').listen(3000);
-
-io.sockets.on('connection', function (socket) {
-
-    socket.on('SADIT', function (data) {
-        console.log("Got data : " + data.value);
-        io.sockets.emit('recv_data', data);
-    });
-});
