fafb33f55d8f870a225ed4e456d7051ab89e26eb
Jing Conan Wang <hbhzwj@gmail.com>
2014-07-18 01:04:45 -0400

    update warning message in Experiment __init__.py

--- Experiment/__init__.py
+++ Experiment/__init__.py
@@ -2,6 +2,7 @@ from .GUITopoSim import GUITopoSim as guitoposim
 from .Sim import Sim as sim
 try:
     from Detector.gad.Experiment import *
-except:
-    print '[warning]: experiments in Detector submodule cannot be initialized'
+except Exception as e:
+    print '[warning]: experiments in Detector submodule cannot be initialized. error:',
+    print e
     pass
