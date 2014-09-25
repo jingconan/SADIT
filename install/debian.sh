8d63f4e237879a1c7cd2fbb9a794d4f424ff41df
jingzbu <jingzbu@gmail.com>
2014-09-25 12:17:22 -0400

    modified the install folder

--- install/debian.sh
+++ install/debian.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 sudo apt-get install python-dev
 sudo apt-get install python-numpy
-#sudo apt-get install python-scipy
+sudo apt-get install python-scipy
 sudo apt-get install python-matplotlib
 sudo apt-get install python-setuptools
 sudo python setup-dep.py
