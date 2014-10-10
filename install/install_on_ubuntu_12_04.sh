a39d254d03e6b4fdbe784252ec6b8b8fe2a24681
jingzbu <jingzbu@gmail.com>
2014-10-10 14:48:16 -0400

    Added automated installation file for Ubuntu 12.04; see install_on_ubuntu_12_04.sh --Jing Zhang (jingzbu@gmail.com)

---
+++ install/install_on_ubuntu_12_04.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+##### Added and tested by Jing Zhang (jingzbu@gmail.com) #####
+
+# cd (path to)/sadit/install/
+sudo apt-get install mercurial
+sudo bash debian.sh 
+sudo apt-get install python-scipy
+sudo sh clean.sh
