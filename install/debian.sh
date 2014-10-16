5b3a1cbbf330fbeca63e43600129a7f821f907d2
jingzbu <jingzbu@gmail.com>
2014-10-16 13:24:51 -0400

    remove installation scripts for Ubuntu 12.04 & 12.04; modified debian.sh

--- install/debian.sh
+++ install/debian.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 sudo apt-get install python-dev
 sudo apt-get install python-numpy
-#sudo apt-get install python-scipy
 sudo apt-get install python-matplotlib
 sudo apt-get install python-setuptools
 sudo python setup-dep.py
+sudo apt-get install python-scipy
+sudo apt-get install python-pydot
+sudo sh clean.sh
